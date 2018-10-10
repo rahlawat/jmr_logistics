fd7bdd70d2d58ea7f499124b9637df8472063f28
Renu Ahlawat <renuahla@thoughtworks.com>
2018-10-10 18:08:01 +0530

    Increasing the size of the truck owner field

--- app/models/truck_details.rb
+++ app/models/truck_details.rb
@@ -2,5 +2,5 @@ class TruckDetails < ApplicationRecord
   validates :truck_number, presence: true,
             length: { maximum: 15 }
   validates :truck_owner, presence: true,
-            length: { maximum: 15 }
+            length: { maximum: 25 }
 end
