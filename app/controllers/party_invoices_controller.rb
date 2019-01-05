b983c9b3087378592eb8a4d514bb7b97a231ac93
Renu Ahlawat <renuahla@thoughtworks.com>
2019-01-05 18:12:36 +0530

    added show for party invoice

--- app/controllers/party_invoices_controller.rb
+++ app/controllers/party_invoices_controller.rb
@@ -2,4 +2,8 @@ class PartyInvoicesController < ApplicationController
   def index
     @party_invoices = PartyInvoice.order(:invoice_number).page params[:page]
   end
+
+  def show
+    @party_invoice = PartyInvoice.find(params[:id])
+  end
 end
