4dbb80fd0fd13b3b0a5583377412f8c886c9c078
Renu Ahlawat <renuahla@thoughtworks.com>
2019-03-28 20:43:51 +0530

    added party code to payment receipt

--- app/models/payment_receipt.rb
+++ app/models/payment_receipt.rb
@@ -1,5 +1,6 @@
 class PaymentReceipt < ApplicationRecord
   validates :amount, presence: true, numericality: true, length: { maximum: 7 }
+  validates :party_code, presence: true
 
   enum mode_of_payment: { imps: 'IMPS/UPI', cash: 'Cash', neft: 'NEFT/RTGS', cheque: 'Cheque' }
   belongs_to :party
