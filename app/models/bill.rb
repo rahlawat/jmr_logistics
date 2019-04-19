69c698f981d7efbff8541fe86d1eeeced2b36c49
Renu Ahlawat <renuahla@thoughtworks.com>
2019-04-19 15:20:56 +0530

    marked bill number as non madatory

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,7 +2,7 @@ class Bill < ApplicationRecord
   validates :weight2, presence: true, numericality: true
   validates :kanta2, presence: true, numericality: true
   validates :tds_percentage, presence: true, numericality: true
-  validates :bill_number, presence: true, length: { maximum: 15 }
+  validates :bill_number, length: { maximum: 15 }
   belongs_to :entry
   belongs_to :party_invoice
 
