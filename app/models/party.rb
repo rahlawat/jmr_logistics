fcefafd7cb59c10958c6a109297d7e8fe45a4c03
Renu Ahlawat <renuahla@thoughtworks.com>
2019-07-20 17:10:54 +0530

    Fixed balance

--- app/models/party.rb
+++ app/models/party.rb
@@ -23,13 +23,13 @@ class Party < ApplicationRecord
     balance = opening_balance[0].balance
     from_date_for_data = DateTime.new(opening_balance[0].year, 4, 1)
     payments = PaymentReceipt.where(:party_code => party_code, :date => from_date_for_data..till_date).order(date: :asc)
-    party_invoices = PartyInvoice.where(:party_code => party_code, :date => from_date_for_data..till_date, :invoice_generated => true)
+    party_invoices = PartyInvoice.where(:party_code => party_code, :date => from_date_for_data..till_date, :invoice_generated => true).order(date: :asc)
     payments.each do |payment|
       balance = balance - payment.amount
     end
 
     party_invoices.each do |party_invoice|
-      balance = balance + (party_invoice.freight_total - (party_invoice.invoice_total * party_invoice.bills[0].tds_percentage/100))
+      balance = balance + (party_invoice.freight_total - (party_invoice.freight_total * party_invoice.bills[0].tds_percentage/100))
     end
     balance
   end
