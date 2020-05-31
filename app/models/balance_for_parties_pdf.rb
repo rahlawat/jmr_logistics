9f61b07bff394f08e237236560e735140d690305
Renu Ahlawat <renuahla@thoughtworks.com>
2020-05-31 15:07:06 +0530

    checking for 1 party

--- app/models/balance_for_parties_pdf.rb
+++ app/models/balance_for_parties_pdf.rb
@@ -6,7 +6,7 @@ class BalanceForPartiesPdf
   def initialize(to_date)
     @to_date = to_date.strftime
     # @parties = Party.order(:party_code)
-    @parties = Party.first.to_a
+    @parties = [Party.first]
   end
 
   def to_pdf
